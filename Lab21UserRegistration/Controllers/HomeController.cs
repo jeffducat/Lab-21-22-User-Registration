e190ca38a56672f92f0d387f7fd3daa97a055c49
jeff_ducat <jducat81@gmail.com>
2019-05-27 16:21:01 -0400

    Lab 21/22 user registration

---
+++ Lab21UserRegistration/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21UserRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string username, string email, string password, string confirmPassword, 
+            string phoneNumber, string city, string country)
+        {
+            ViewBag.UserName = username;
+            ViewBag.Email = email;
+            ViewBag.Password = password;
+            ViewBag.ConfirmPassword = confirmPassword;
+            ViewBag.PhoneNumber = phoneNumber;
+            ViewBag.City = city;
+            ViewBag.Country = country;
+
+            return View(); 
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+       
+    }
+}
