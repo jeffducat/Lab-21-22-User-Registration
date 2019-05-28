f8d45929703b07f51e14cdb70747bc1f26204702
jeff_ducat <jducat81@gmail.com>
2019-05-28 13:08:06 -0400

    updates to models

---
+++ Lab21UserRegistration/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab21UserRegistration.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Country{ get; set; }
+        public string City { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string userName, string country, string city, string password, string email)
+        {
+            UserName = userName;
+            Country = country;
+            City = city;
+            Password = password;
+            Email = email;
+        }
+    }
+}
